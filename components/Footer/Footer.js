11575ce81a3409a154de78f8978998d0dab3700e
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2024-09-18 09:09:07 +0100

    Handling About Page responsiveness

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,31 +7,31 @@ const Footer = () => {
   const pathname = usePathname();
   return (
     <div className=" w-[95%] mx-auto border-t border-t-[#adb5bd] space-y-12">
-      <div className="flex justify-between mt-20">
-        <div className="flex gap-x-48">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between mt-20 md:space-y-0 space-y-5">
+        <div className="flex flex-col md:flex-row md:items-center md:gap-x-12 md:space-y-0 space-y-5">
           <ul className="block" data-aos="zoom-in-up">
             <li>
               <Link
                 href={`/about`}
-                className={`block text-[var(--faded-text)] text-[28px] pb-2 ${pathname === `/about` ? "text-[var(--foreground)] font-extrabold" : ""}`}
+                className={`block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2 ${pathname === `/about` ? "text-[var(--foreground)] font-extrabold" : ""}`}
               >
                 About
               </Link>
               <Link
                 href={`/projects`}
-                className={`block text-[var(--faded-text)] text-[28px] pb-2 ${pathname === `/projects` ? "text-[var(--foreground)] font-extrabold" : ""}`}
+                className={`block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2 ${pathname === `/projects` ? "text-[var(--foreground)] font-extrabold" : ""}`}
               >
                 Projects
               </Link>
               <Link
                 href={`/blog`}
-                className={`block text-[var(--faded-text)] text-[28px] pb-2 ${pathname === `/blog` ? "text-[var(--foreground)] font-extrabold" : ""}`}
+                className={`block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2 ${pathname === `/blog` ? "text-[var(--foreground)] font-extrabold" : ""}`}
               >
                 Project
               </Link>
               <Link
                 href={`/contact`}
-                className={`block text-[var(--faded-text)] text-[28px] pb-2 ${pathname === `/contact` ? "text-[var(--foreground)] font-extrabold" : ""}`}
+                className={`block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2 ${pathname === `/contact` ? "text-[var(--foreground)] font-extrabold" : ""}`}
               >
                 Contact
               </Link>
@@ -43,28 +43,28 @@ const Footer = () => {
               <Link
                 href={`/https://github.com/Muhtoyyib`}
                 target="_blank"
-                className="block text-[var(--faded-text)] text-[28px] pb-2"
+                className="block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2"
               >
                 GitHub
               </Link>
               <Link
                 href={`https://www.linkedin.com/in/akande-olalekan-toheeb-2a69a0221x.com`}
                 target="_blank"
-                className="block text-[var(--faded-text)] text-[28px] pb-2"
+                className="block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2"
               >
                 LinkedIn
               </Link>
               <Link
                 href={`https://x.com/devtoheeb`}
                 target="_blank"
-                className="block text-[var(--faded-text)] text-[28px] pb-2"
+                className="block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px] pb-2"
               >
                 Twitter/X
               </Link>
               <Link
                 href={`https://www.freecodecamp.org/news/author/MuhToyyib/`}
                 target="_blank"
-                className="block text-[var(--faded-text)] text-[28px]"
+                className="block text-[var(--faded-text)] hover:text-[var(--nav-color)] text-[28px]"
               >
                 FreeCodeCamp
               </Link>
@@ -73,7 +73,7 @@ const Footer = () => {
         </div>
 
         <p
-          className="text-[var(--faded-text)] text-2xl pb-2"
+          className="text-[var(--faded-text)] hover:text-[var(--nav-color)] text-2xl pb-2"
           data-aos="zoom-in-up"
         >
           Copyright &copy; 2024 Muh.Toyyib
@@ -81,7 +81,7 @@ const Footer = () => {
       </div>
 
       <h2
-        className="text-center text-[80px]"
+        className="text-center md:text-[80px] text-[22px]"
         data-aos="fade-up"
         data-aos-anchor-placement="center-bottom"
       >
--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,66 +1,109 @@
-import React from "react";
+"use client";
 
+import { useState } from "react";
+import { sendForm } from "@emailjs/browser";
 const Form = () => {
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    // Put the sendForm inside a setTimeout
+    sendForm(
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+      e.target,
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+    ).then(
+      (result) => {
+        console.log(result.text);
+        setStatus("SUCCESS");
+      },
+      (error) => {
+        console.log(error.text);
+        setStatus("ERROR");
+      }
+    );
+
+    setLoading(false);
+    e.target.reset();
+  };
   return (
     <div>
-      <h3 className="text-2xl uppercase pb-5">Contact detials</h3>
+      <h3 className="text-[20px] uppercase pb-5">Contact detials</h3>
 
-      {/* Name */}
-      <div className="flex justify-between py-7 px-10 bg-[var(--background-reverse)] rounded-lg text-2xl mb-5">
-        <label htmlFor="first_name">First Name</label>
-        <input
-          type="text"
-          id="first_name"
-          name="first_name"
-          placeholder="Akande Olalekan Toheeb"
-          className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
-        />
-      </div>
+      {status === "SUCCESS" && (
+        <p className="pb-3 text-green-700 text-[16px]">
+          Thanks for your message!
+        </p>
+      )}
+      {status === "ERROR" && (
+        <p className="pb-3 text-red-700 text-[16px]">
+          Oops, something went wrong. Please try again.
+        </p>
+      )}
 
-      {/* Company */}
-      <div className="flex justify-between py-7 px-10 bg-[var(--background-reverse)] rounded-lg text-2xl mb-5">
-        <label htmlFor="last_name">Last Name</label>
-        <input
-          type="text"
-          id="last_name"
-          name="last_name"
-          placeholder="Akande Olalekan Toheeb"
-          className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
-        />
-      </div>
+      <form onSubmit={handleSubmit}>
+        {/* Name */}
+        <div className="flex justify-between py-4 px-7 bg-[var(--background-reverse)] rounded-lg text-[20px] mb-5">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            placeholder=" Olalekan Toheeb"
+            className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
+          />
+        </div>
 
-      {/* Email */}
-      <div className="flex justify-between py-7 px-10 bg-[var(--background-reverse)] rounded-lg text-2xl mb-5">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="akandeolalekantoheeb94@gmail.com"
-          className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
-        />
-      </div>
+        {/* Company */}
+        <div className="flex justify-between py-4 px-7 bg-[var(--background-reverse)] rounded-lg text-[20px] mb-5">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            placeholder="Akande"
+            className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
+          />
+        </div>
 
-      {/* Message */}
-      <div className="py-5 px-10 bg-[var(--background-reverse)] rounded-lg text-2xl">
-        <label htmlFor="message" className="block mb-1">
-          Message
-        </label>
-        <textarea
-          type="text"
-          id="message"
-          name="message"
-          placeholder="How can I help?"
-          className="border-none outline-none bg-transparent text-[var(--foreground)] w-full h-40"
-        ></textarea>
-      </div>
+        {/* Email */}
+        <div className="flex justify-between py-4 px-7 bg-[var(--background-reverse)] rounded-lg text-[20px] mb-5">
+          <label htmlFor="reply_to">Email</label>
+          <input
+            type="email"
+            id="reply_to"
+            name="reply_to"
+            placeholder="akandeolalekantoheeb94@gmail.com"
+            className="border-none outline-none bg-transparent text-[var(--foreground)] w-4/5 text-right"
+          />
+        </div>
 
-      <button
-        type="submit"
-        className="py-5 text-[24px] px-10 rounded-[45px] bg-[var(--foreground)] text-[var(--background)] float-end mt-7 hover:bg-[var(--background-reverse)] hover:text-[var(--foreground)]"
-      >
-        Submit request
-      </button>
+        {/* Message */}
+        <div className="py-4 px-7 bg-[var(--background-reverse)] rounded-lg text-[20px]">
+          <label htmlFor="message" className="block mb-1">
+            Message
+          </label>
+          <textarea
+            type="text"
+            id="message"
+            name="message"
+            placeholder="How can I help?"
+            className="border-none outline-none bg-transparent text-[var(--foreground)] w-full h-30"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="py-3 px-7 rounded-[45px] bg-[var(--foreground)] text-[var(--background)] float-end mt-5 hover:bg-[var(--background-reverse)] hover:text-[var(--foreground)]"
+        >
+          {loading ? "Submitting request..." : "Submit request"}
+        </button>
+      </form>
     </div>
   );
 };
